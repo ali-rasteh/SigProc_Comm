2fdd55cb4e1b49b955292b9f3937f48fb2c50c68
ali-rasteh <ali.rasteh1@gmail.com>
2025-02-11 14:39:42 -0500

    Debugged similarity computation

--- signal_utils.py
+++ signal_utils.py
@@ -643,17 +643,26 @@ class Signal_Utils(General):
 
     def compute_slices_similarity(self, predicted, target):
         if predicted is None and target is not None:
-            det_rate = 0.0
+            det_rate = None
             missed = 1.0
-            false_alarm = 0.0
+            false_alarm = None
+            # det_rate = 0.0
+            # missed = 1.0
+            # false_alarm = 0.0
         elif target is None and predicted is not None:
-            det_rate = 0.0
-            missed = 0.0
+            det_rate = None
+            missed = None
             false_alarm = 1.0
+            # det_rate = 0.0
+            # missed = 0.0
+            # false_alarm = 1.0
         elif predicted is None and target is None:
-            det_rate = 1.0
-            missed = 0.0
+            det_rate = None
+            missed = None
             false_alarm = 0.0
+            # det_rate = 1.0
+            # missed = 0.0
+            # false_alarm = 0.0
         else:
             intersection = self.slice_intersection(predicted, target)
             union = self.slice_union(predicted, target)
@@ -664,7 +673,10 @@ class Signal_Utils(General):
             # det_rate = intersection_size / max_size
             det_rate = intersection_size / union_size
             missed = 0.0
-            false_alarm = 0.0
+            false_alarm = None
+            # det_rate = intersection_size / union_size
+            # missed = 0.0
+            # false_alarm = 0.0
 
         return (det_rate, missed, false_alarm)
     
